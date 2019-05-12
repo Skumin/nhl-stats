1c064ca95cbd9cbbcf672ef72d4593a3ec35add2
Skumin <polach.honza@gmail.com>
2019-05-12 19:28:09 +0100

    Added Live Feed shots.

--- new_api_func.jl
+++ new_api_func.jl
@@ -38,7 +38,8 @@ function dressed_skaters(box_score_output, side)
     return f
 end
 
-function dropnames(namedtuple::NamedTuple, names::Tuple{Vararg{Symbol}}) # This deletes a tuple from a NamedTuple by name
+# This deletes a tuple from a NamedTuple by name
+function dropnames(namedtuple::NamedTuple, names::Tuple{Vararg{Symbol}})
     keepnames = Base.diff_names(Base._nt_names(namedtuple), names)
     return NamedTuple{keepnames}(namedtuple)
 end
@@ -57,4 +58,51 @@ function stats_dressed_skaters(box_score_output, side)
     player_sts = [player_summary(plr) for plr in tm]
     r = NamedTuple{Tuple(player_ids)}(player_sts)
     return r
+end
+
+function shots_lf(live_feed_output)
+    shots = [evnts for evnts in pairs(live_feed_output[:liveData][:plays][:allPlays]) if evnts[2][:result][:event] in ["Shot", "Goal"]]
+
+    shtr = DataFrame([dropnames(evnt[2][:players][1][:player], (:link, )) for evnt in shots])
+    rename!(shtr, :id => :idShooter)
+    rename!(shtr, :fullName => :fullNameShooter)
+
+    tm = DataFrame([dropnames(evnt[2][:team], (:link, :triCode, )) for evnt in shots])
+    rename!(tm, :id => :idTeam)
+    rename!(tm, :name => :nameTeam)
+
+    tm[:shotNumber] = 1:nrow(tm)
+
+    goalie = DataFrame([dropnames(evnt[2][:players][2][:player], (:link, )) for evnt in shots])
+    rename!(goalie, :id => :idGoalie)
+    rename!(goalie, :fullName => :fullNameGoalie)
+
+    fullframe = hcat(shtr, tm)
+    fullframe = hcat(fullframe, goalie)
+
+    fullframe[:shotType] = [evnt[2][:result][:secondaryType] for evnt in shots]
+
+    whn = DataFrame([dropnames(evnt[2][:about], (:eventIdx, :eventId, :ordinalNum, :periodTimeRemaining, :goals, )) for evnt in shots])
+    fullframe = hcat(fullframe, whn)
+
+    fullframe[:goalsAway] = [evnt[2][:about][:goals][:away] for evnt in shots]
+    fullframe[:goalsHome] = [evnt[2][:about][:goals][:home] for evnt in shots]
+    fullframe[:coordX] = [evnt[2][:coordinates][:x] for evnt in shots]
+    fullframe[:coordY] = [evnt[2][:coordinates][:y] for evnt in shots]
+
+    isgoal = falses(nrow(fullframe))
+    for i = 1:length(isgoal)
+        if i == 1
+            if (fullframe[1, :goalsAway] == 1) | (fullframe[1, :goalsHome] == 1)
+                isgoal[1] = true
+            end
+        else
+            if (fullframe[i, :goalsAway] != fullframe[i - 1, :goalsAway]) | (fullframe[i, :goalsHome] != fullframe[i - 1, :goalsHome])
+                isgoal[i] = true
+            end
+        end
+    end
+    fullframe[:isGoal] = isgoal
+
+    return fullframe
 end
